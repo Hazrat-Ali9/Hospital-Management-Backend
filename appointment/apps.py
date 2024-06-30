900e1a22235f0e1370c010058cb4121f38f11468
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-06-30 20:42:32 +0600

    appoinment config paitent :racehorse

--- appointment/apps.py
+++ appointment/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# apps config appionment
 class AppointmentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appointment'
