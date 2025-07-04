4f8b92979bb63ae38d08c880ddf501b127f2109e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-04 16:11:53 +0600

    models 🏀

--- patient/models.py
+++ patient/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+# models
 # Create your models here.
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
